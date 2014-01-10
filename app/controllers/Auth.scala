841af8eef9ae1b6cdcbbeeea0acf66f5c368b461
eric-wu <eric.wu@sagebase.org>
2014-01-09 22:51:31 -0800

    Add a very simple authorization based on email domain.

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -1,22 +1,26 @@
 package controllers
 
+import collection.JavaConversions._
 import play.api.mvc._
-
 import org.openid4java.consumer.ConsumerManager
 import org.openid4java.message.AuthRequest
+import org.openid4java.message.AuthSuccess
+import org.openid4java.message.ParameterList
+import org.openid4java.message.ax.AxMessage
 import org.openid4java.message.ax.FetchRequest
+import org.openid4java.message.ax.FetchResponse
 
 object Auth extends Controller {
 
   private val providerUrl = "https://www.google.com/accounts/o8/id"
-  private val openIdMgr = new ConsumerManager()
+  private val openIdMgr = new ConsumerManager
   private val discovered = openIdMgr.associate(openIdMgr.discover(providerUrl))
 
   /**
-   * Logs in the user with Google OpenID 2.0.
+   * Logs in the user using Google OpenID 2.0.
    */
   def login = Action { implicit request =>
-    val returnToUrl = routes.Auth.authenticate.absoluteURL(false)
+    val returnToUrl = routes.Auth.auth.absoluteURL(false)
     val authReq = openIdMgr.authenticate(discovered, returnToUrl)
     val fetchReq = FetchRequest.createFetchRequest
     fetchReq.addAttribute(
@@ -28,16 +32,36 @@ object Auth extends Controller {
   }
 
   /**
-   * Authenticates based on the 'sagebase.org' email domain.
+   * Authenticates and authorizes.
    */
-  def authenticate = Action { request =>
-    Ok("authenticate")
+  def auth = Action { implicit request =>
+    val receivingURL = "http://" + request.host + request.uri
+    val response = new ParameterList(request.queryString.map { case (k,v) => k -> v.mkString })
+    val verification = openIdMgr.verify(
+        receivingURL.toString(),
+        response, discovered)
+    val verifiedId = verification.getVerifiedId()
+    if (verifiedId == null) {
+      Forbidden("Log in failed.")
+    } else {
+      val authSuccess = verification.getAuthResponse().asInstanceOf[AuthSuccess]
+      val fetchResp = authSuccess.getExtension(AxMessage.OPENID_NS_AX).asInstanceOf[FetchResponse]
+      val emails = fetchResp.getAttributeValues("email")
+      authorize(emails.get(0).toString)
+    }
   }
 
   /**
    * Authorizes based on Synapse user info.
    */
-  def authorize = Action { request =>
-    Ok("authorize")
+  def authorize(email: String): Result = {
+    if (email == null) {
+      Unauthorized("Missing email address.")
+    } else if (email.toLowerCase().endsWith("@sagebase.org")) {
+      // TODO: Cross-check with Synapse for tighter security
+      Ok("authorized")
+    } else {
+      Unauthorized(email + " is not authorized.");
+    }
   }
 }
