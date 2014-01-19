93bbcf660250fb8966f7ffd06f6d9781d4db6955
eric-wu <eric.wu@sagebase.org>
2014-01-18 22:36:56 -0800

    Add comment.

--- app/Global.scala
+++ app/Global.scala
@@ -1,6 +1,10 @@
 import play.api.{GlobalSettings, Logger}
 import context.SpringContext
 
+// Notes:
+// 1) This must be in the default package.
+// 2) There is a corresponding change in application.conf.
+// 3) The DEV mode (the run command) does not support this.
 object Global extends GlobalSettings {
 
   override def onStart(app: play.api.Application) {
