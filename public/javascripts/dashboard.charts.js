374ff7f148b564b77d73fa28a5ec5bb51a24f8c8
eric-wu <eric.wu@sagebase.org>
2014-02-11 16:49:53 -0800

    Rewrite hbar and line.

--- public/javascripts/dashboard.charts.js
+++ public/javascripts/dashboard.charts.js
@@ -50,8 +50,12 @@ dashboard.charts = (function() {
     chart = addSvg(width, height)
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    xAxis = addAxisX(chart, x, height - margin.top - margin.bottom);
-    yAxis = addAxisY(chart, y);
+    if (x) {
+      xAxis = addAxisX(chart, x, height - margin.top - margin.bottom);
+    }
+    if (y) {
+      yAxis = addAxisY(chart, y);
+    }
     plot = chart.selectAll(".plot")
       .data(data)
       .enter().append("g")
@@ -71,7 +75,7 @@ dashboard.charts = (function() {
   //===============================
   bar = function(data, width, height, margin) {
 
-    var w, h, x0, x1, xAxis, y, yAxis,
+    var w, h, xScale0, xScale1, xAxis, y, yAxis,
         svg, chart, plot, color, legend;
 
     // Remove any existing chart
@@ -88,12 +92,12 @@ dashboard.charts = (function() {
     h = height - margin.top - margin.bottom;
 
     // The x-axis
-    x0 = d3.scale.ordinal()
+    xScale0 = d3.scale.ordinal()
       .domain(data.xSeries)
       .rangeRoundBands([0, w], 0.2);
 
     xAxis = d3.svg.axis()
-      .scale(x0)
+      .scale(xScale0)
       .orient('bottom');
 
     // At maximum, display 12 ticks on the x-axis
@@ -117,19 +121,19 @@ dashboard.charts = (function() {
     svg = addChart(data.xGroups, width, height, margin, xAxis, yAxis);
     chart = svg.chart;
     plot = svg.plot;
-    plot.attr("transform", function(d) { return "translate(" + x0(d.x) + ",0)"; });
+    plot.attr("transform", function(d) { return "translate(" + xScale0(d.x) + ",0)"; });
 
-    x1 = d3.scale.ordinal()
+    xScale1 = d3.scale.ordinal()
       .domain(data.headers)
-      .rangeRoundBands([0, x0.rangeBand()]);
+      .rangeRoundBands([0, xScale0.rangeBand()]);
 
     color = d3.scale.category10().domain(data.headers.concat("__hover__"));
 
     plot.selectAll("rect")
       .data(function(group) { return group.values; })
       .enter().append("rect")
-      .attr("width", x1.rangeBand())
-      .attr("x", function(d) { return x1(d.header); })
+      .attr("width", xScale1.rangeBand())
+      .attr("x", function(d) { return xScale1(d.header); })
       .attr("y", function(d) { return y(d.y); })
       .attr("height", function(d) { return h - y(d.y); })
       .style("fill", function(d) { return color(d.header); })
@@ -139,10 +143,10 @@ dashboard.charts = (function() {
           .text(d.y)
           .attr("id", "hovertext")
           .attr("text-anchor", "middle")
-          // TODO: The line below should use "x1(d.header)".
+          // TODO: The line below should use "xScale1(d.header)".
           // But currently the header is empty.
-          // So "x0(d.x)" is used here as a temporary hack.
-          .attr("x", x0(d.x) + x1.rangeBand() / 2)
+          // So "xScale0(d.x)" is used here as a temporary hack.
+          .attr("x", xScale0(d.x) + xScale1.rangeBand() / 2)
           .attr("y", y(d.y) - 10)
           .attr("fill", "black");
       })
@@ -151,6 +155,7 @@ dashboard.charts = (function() {
         chart.select("#hovertext").remove();
       });
 
+    // Legend
     legend = chart.selectAll(".legend")
       .data(data.headers.slice())
       .enter().append("g")
@@ -169,20 +174,182 @@ dashboard.charts = (function() {
       .text(function(d) { return d; });
   };
 
+  //=============================================
+  //Renders a grouped horizontal bar chart.
+  //=============================================
   hbar = function(data, width, height, margin) {
+
+    var w, h, xScale, xAxis, yScale0, yScale1,
+        svg, chart, plot, color;
+
+    // Remove any existing chart
+    removeSvg();
+
     // Empty data set
     if (isEmptyData(data)) {
       addEmptyChart(width, height);
       return;
     }
+
+    // Compute chart width and height
+    w = width - margin.left - margin.right;
+    h = height - margin.top - margin.bottom;
+
+    // The x-axis
+    xScale = d3.scale.log()
+      .domain([1, data.yMax])
+      .range([0, w]);
+
+    xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("top")
+      .ticks(5, 'd'); // Ticks are 5 numbers
+
+    // The y scales. Note there is no y-axis on the hbar chart.
+    yScale0 = d3.scale.ordinal()
+      .domain(data.xSeries)
+      .rangeRoundBands([0, h], 0.2);
+
+    yScale1 = d3.scale.ordinal()
+      .domain(data.headers)
+      .rangeRoundBands([0, yScale0.rangeBand()]);
+
+    // The plot
+    svg = addChart(data.xGroups, width, height, margin, xAxis, null);
+
+    plot = svg.plot;
+    plot.attr("transform", function(d) { return "translate(0," + yScale0(d.x) + ")"; });
+
+    color = d3.scale.category10().domain(data.headers);
+
+    plot.selectAll("rect")
+      .data(function(group) { return group.values; })
+      .enter().append("rect")
+      .attr("class", "rect")
+      .attr("x", function(d) { return xScale(1); })
+      .attr("y", function(d) { return yScale1(d.header); })
+      .attr("width", function(d) { return xScale(d.y); })
+      .attr("height", yScale1.rangeBand())
+      .style("fill", function(d) { return color(d.header); });
+
+    plot.selectAll(".score")
+      .data(function(group) { return group.values; })
+      .enter().append("text")
+      .attr("x", function(d) { return xScale(d.y); })
+      .attr("y", function(d) { return yScale1(d.header) + yScale1.rangeBand() / 2; })
+      .attr("dx", 10)
+      .attr("dy", ".36em")
+      .attr("text-anchor", "start")
+      .attr("class", "score")
+      .text(function(d) { return d.y; });
+
+    // Labels on the left side
+    chart = svg.chart;
+    chart.selectAll(".label")
+      .data(data.xGroups)
+      .enter().append("text")
+      .attr("x", -10)
+      .attr("y", function(group) { return yScale0(group.x) + yScale0.rangeBand() / 2; })
+      .attr("dy", ".36em")
+      .attr("text-anchor", "end")
+      .attr("class", "barLabel")
+      .text(function(group) {
+        var txt = group.x;
+        // TODO: Replace the magic numbers. Hook them up instead to width or margin
+        if (txt.length > 36) {
+          txt = txt.substring(0, 33);
+          txt = txt + "...";
+        }
+        return txt;
+      })
+      .on("mouseover", function(d) {
+        if (d.x.length > 36) {
+          chart.append("text")
+            .text(d.x)
+            .attr("id", "hovertext")
+            .attr("text-anchor", "start")
+            .attr("x", 10)
+            .attr("y", yScale0(d.x) + yScale0.rangeBand() / 2)
+            .attr("dy", ".36em")
+            .attr("fill", "orange");
+        }
+      })
+      .on("mouseout", function(d) { chart.select("#hovertext").remove(); });
   };
 
   line = function(data, width, height, margin) {
+
+    var w, h, xScale, xAxis, yScale, yAxis,
+        svg, plot, line, color, chart;
+
+    // Remove any existing chart
+    removeSvg();
+
     // Empty data set
     if (isEmptyData(data)) {
       addEmptyChart(width, height);
       return;
     }
+
+    // Compute chart width and height
+    w = width - margin.left - margin.right;
+    h = height - margin.top - margin.bottom;
+
+    // The x-axis
+    xScale = d3.time.scale()
+      .range([0, w])
+      .domain([data.xMin, data.xMax]);
+
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+    // The y-axis
+    yScale = d3.scale.linear()
+      .range([h, 0])
+      .domain([data.yMin, data.yMax]);
+
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+    // The plot
+    svg = addChart(data.ySeries, width, height, margin, xAxis, yAxis);
+    plot = svg.plot;
+
+    line = d3.svg.line()
+      .interpolate("linear")
+      .x(function(d) { return xScale(d.x); })
+      .y(function(d) { return yScale(d.y); });
+
+    color = d3.scale.category10().domain(data.headers);
+
+    plot.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); })
+      .style("stroke", function(d) { return color(d.header); });
+
+    plot.append("text")
+      .datum(function(oneSeries) {
+        return {
+          header: oneSeries.header,
+          lastVal: oneSeries.values[oneSeries.values.length - 1]}; })
+      .attr("transform", function(d) {
+          return "translate(" + xScale(d.lastVal.x) + ","
+            + yScale(d.lastVal.y) + ")"; })
+      .attr("x", 5)
+      .attr("dy", 10)
+      .text(function(d) { return d.header; });
+
+    // The y-axis label
+    chart = svg.chart;
+    chart.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 10)
+      .attr("dy", 10)
+      .style("text-anchor", "end")
+      .text(data.yLabel);
+
+    // Reset the max on y ticks
+    chart.selectAll(".y.axis g text").forEach(function(tickText) {
+      console.debug(tickText);
+    });
   };
 
   return {
